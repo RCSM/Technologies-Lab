296f278ed08c5c063921cd2ff306522570f6942b
Rafael Cascalho <cascalhoeng@gmail.com>
2017-11-27 13:13:03 -0200

    Removendo antigo projeto de Django

--- Python/Django First App/randGenSite/randGenSite/firstView.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from random import randint
-
-@require_http_methods("GET ")
-def rangGen():
-    num = randint(0, 99999)
-    html = "<html><body></body>%s</html>" % num
-    return HttpResponse(html)
