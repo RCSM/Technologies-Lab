2d36e4479d264aea5cb9c0cd232e1cb953f0bab2
Rafael Cascalho <cascalhoeng@gmail.com>
2018-09-03 10:37:38 -0300

    Update project

--- Python/Django/Crudv1.0/crudv1/urls.py
+++ Python/Django/Crudv1.0/crudv1/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from crudv1.login import views as login_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
