a0697d85239a95b105c7a244cb812dc61319b2ad
Rafael Cascalho <cascalhoeng@gmail.com>
2018-09-12 01:43:23 -0300

    Add groups permissions

--- Python/Django/djauth/users/models.py
+++ Python/Django/djauth/users/models.py
@@ -4,5 +4,12 @@ from django.db import models
 class CustomUser(AbstractUser):
     # Add additional fields here
 
+    class Meta():
+        permissions = (
+            ('G1', 'Pode ver x'),
+            ('G2', 'Pode ver y'),
+            ('G3', 'Pode ver z')
+        )
+
     def __str__(self):
         return self.email
